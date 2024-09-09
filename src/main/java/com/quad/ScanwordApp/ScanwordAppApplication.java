ee04146c1797dfabd517b82770299cd6bc5a209c
Yuriy <valyur2003@yandex.ru>
2024-09-09 14:32:51 +0400

    init

---
+++ src/main/java/com/quad/ScanwordApp/ScanwordAppApplication.java
@@ -0,0 +1,13 @@
+package com.quad.ScanwordApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanwordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanwordAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quad/ScanwordApp/ScanwordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quad.ScanwordApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanwordAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
