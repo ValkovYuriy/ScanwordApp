6ed2e2868ff21d3834318da4f2f9da2a4343624d
Yuriy <valyur2003@yandex.ru>
2024-10-19 12:56:38 +0400

    small fixes

---
+++ src/main/java/com/quad/ScanwordApp/controller/ErrorHandler.java
@@ -0,0 +1,24 @@
+package com.quad.ScanwordApp.controller;
+
+
+import com.quad.ScanwordApp.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+
+    @ExceptionHandler
+    public ErrorResponse handleNotFoundException(NotFoundException e) {
+        log.error(e.getMessage(),e);
+        return new ErrorResponse("ОК",e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ErrorResponse handleException(Throwable e) {
+        log.error(e.getMessage(),e);
+        return new ErrorResponse("ОК",e.getMessage());
+    }
+}
--- src/main/java/com/quad/ScanwordApp/exception/NotFoundException.java
+++ src/main/java/com/quad/ScanwordApp/exception/NotFoundException.java
@@ -1,4 +1,6 @@
 package com.quad.ScanwordApp.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message){super(message);}
 }
