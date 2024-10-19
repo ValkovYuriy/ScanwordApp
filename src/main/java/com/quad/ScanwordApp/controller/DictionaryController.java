1a9e90075c8a5c3a5b7ea1e1ab80d0e435673e67
Admin <kashapovh_c@inbox.ru>
2024-10-19 18:38:25 +0400

    Исправления

--- src/main/java/com/quad/ScanwordApp/controller/DictionaryController.java
+++ src/main/java/com/quad/ScanwordApp/controller/DictionaryController.java
@@ -4,17 +4,13 @@ import com.quad.ScanwordApp.dto.DictionaryDto;
 import com.quad.ScanwordApp.service.DictionaryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/dictionary")
 @RequiredArgsConstructor
 public class DictionaryController {
 
--- src/main/java/com/quad/ScanwordApp/controller/MelodyController.java
+++ src/main/java/com/quad/ScanwordApp/controller/MelodyController.java
@@ -4,19 +4,14 @@ package com.quad.ScanwordApp.controller;
 import com.quad.ScanwordApp.service.MelodyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.quad.ScanwordApp.dto.MelodyDto;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/melody")
 @RequiredArgsConstructor
 public class MelodyController {
     
