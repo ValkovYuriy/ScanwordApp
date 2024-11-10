34ed1d9e718d84acb19335b499c345ca8aa3409e
Yuriy <valyur2003@yandex.ru>
2024-11-10 15:40:04 +0400

    fix: Ну рефакторинг типо 3

--- src/main/java/com/quad/ScanwordApp/util/Util.java
+++ src/main/java/com/quad/ScanwordApp/util/Util.java
@@ -1,5 +1,10 @@
 package com.quad.ScanwordApp.util;
 
+import com.quad.ScanwordApp.model.Dictionary;
+import com.quad.ScanwordApp.model.json.DictionaryData;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -11,28 +16,53 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Util {
 
     private static JdbcTemplate jdbcTemplate;
+    private static SessionFactory sessionFactory;
 	private static final String DB_URL = "jdbc:postgresql://localhost:5432/ScanwordApp";
 	private static final String USER = "ydek";
 	private static final String PASS = "remote";
 
-    public static void connect(){
-
+    public static void connectWithJdbcTemplate(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl(DB_URL);
 		dataSource.setUsername(USER);
 		dataSource.setPassword(PASS);
-
 		jdbcTemplate = new JdbcTemplate(dataSource);
-
     }
 
-    public static void insertData(){
+    public static void connectWithHibernate(){
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(Dictionary.class);
+        config.configure();
+        sessionFactory = config.buildSessionFactory();
+    }
+
+    public static void insertDataUsingHibernate() throws IOException {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<DictionaryData> dictionaryData = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/resources/dictionaries/clean_dictionaries/Культура_и_Социум.dict")));
+        while(br.ready()){
+            String[] line = br.readLine().split(" ",2);
+            dictionaryData.add(new DictionaryData(line[0], line[1]));
+        }
+        Dictionary dictionary = Dictionary.builder()
+                .name("Культура и Социум")
+                .dictionaryData(dictionaryData)
+                .build();
+        session.save(dictionary);
+        session.getTransaction().commit();
+    }
+
+    public static void insertDataUsingJdbcTemplate(){
 
 //        Path directoryPath = Paths.get("src/main/resources/images");
 //		BufferedReader bf = new BufferedReader(new FileReader("src/main/resources/dataForImages.txt"));
@@ -77,10 +107,9 @@ public class Util {
 //		}
     }
 
-    public static void clearing(){
+    public static void clearingDictionaries(){
         Path dirtyDictionaries = Paths.get("src/main/resources/dictionaries/dirty_dictionaries");
         Path cleanDictionaries = Paths.get("src/main/resources/dictionaries/clean_dictionaries");
-
 
         try {
             Files.list(dirtyDictionaries).forEach(dirtyDictionary ->{
@@ -93,7 +122,7 @@ public class Util {
                     while(br.ready()){
                         String[] line = br.readLine().split(" ",2);
                         if(uniqueLines.add(line[0])){
-                            fw.write(line[0] + " " + line[1] + "\n");
+                            fw.write(line[0] + " " + line[1].trim() + "\n");
                         }
                     }
                 }
