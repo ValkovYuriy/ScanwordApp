da37b6a5fa073292239889f1be2cb12024f64179
Admin <kashapovh_c@inbox.ru>
2024-12-13 20:34:35 +0400

    Наконец-таки добавил сохранение сканворда в бд(с превью).

--- src/main/java/com/quad/ScanwordApp/util/Util.java
+++ src/main/java/com/quad/ScanwordApp/util/Util.java
@@ -10,11 +10,7 @@ import org.hibernate.cfg.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
