cbced35c2ff3cfb5d2edf07af5c23762681a2c99
Admin <kashapovh_c@inbox.ru>
2024-12-21 18:23:02 +0400

    fix: Добавил роли в модель для отображения информации по ролям

--- src/main/java/com/quad/ScanwordApp/controller/web/HelpController.java
+++ src/main/java/com/quad/ScanwordApp/controller/web/HelpController.java
@@ -1,7 +1,11 @@
 package com.quad.ScanwordApp.controller.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +16,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelpController {
 
     @GetMapping
-    public String helpGet(){
+    public String helpGet(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null && auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken))
+        {
+            if(auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN")))
+            {
+                model.addAttribute("isAdmin", true);
+            }
+            else {
+                model.addAttribute("isUser", true);
+            }
+        }
+        else {
+            model.addAttribute("isAuthenticated", false);
+        }
         return "help";
     }
     @PostMapping
